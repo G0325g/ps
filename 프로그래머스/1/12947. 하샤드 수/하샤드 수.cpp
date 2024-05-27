2b906bc403c52576786193996d9e85d1b9f532a3
G0325g <119734946+G0325g@users.noreply.github.com>
2024-05-27 12:28:28 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    
+    string str = to_string(x);
+    int num=0;
+    for(int i=0; i<str.length();i++)
+    {
+        num+= str[i]-'0';
+    }
+    if(x%num!=0) answer = false;
+        
+    return answer;
+}
